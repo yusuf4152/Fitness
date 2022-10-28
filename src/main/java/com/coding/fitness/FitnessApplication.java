671d67c697b39e291cd83d526c61395fcef2acff
yusuf islam demir <yusufislamdemir41@gmail.com>
2022-10-28 19:59:26 +0300

    initialize

---
+++ src/main/java/com/coding/fitness/FitnessApplication.java
@@ -0,0 +1,13 @@
+package com.coding.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coding/fitness/FitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
